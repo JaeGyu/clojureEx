81907b2e2428a082259558e9d93bb2ad68a468f6
Jaegyu <jpmlyr@gmail.com>
2016-04-27 17:52:09 +0900

    클로저 시작하기 연습

---
+++ 20160427.clj
@@ -0,0 +1,24 @@
+(*)
+
+{:jam1 "ff"}
+
+{"a" "a"}
+
+{"1" 1}
+
+{"key" "value"}
+
+{:key "value"}
+
+{:a "a", :b "b"}
+
+
+(get {"name" "ejb"} "name")
+
+(println (get {:name "ejb"} :name))
+
+
+(get {:a "a" :b "b"} :c "not found")
+
+
+

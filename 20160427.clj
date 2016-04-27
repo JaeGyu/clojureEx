baa210cc8df629547534d7d496c9875f70b74f8d
jpmlyr <jpmlyr@gmail.com>
2016-04-27 23:53:39 +0900

    keys, vals 함수 를 적용한 맵

--- 20160427.clj
+++ 20160427.clj
@@ -21,4 +21,12 @@
 (get {:a "a" :b "b"} :c "not found")
 
 
+;키워드를 함수 처럼 사용해서 map에서 값을 가져 올 수 있다.
+(:jam2 {:jam1 "strawberry" :jam2 "blackberry" :jam3 "marmalade"})
+
+(keys {:jam1 "aa" :jam2 "bb"})
+
+(vals {:jam1 "aa" :jam2 "bb"})
+
+
 
